3668880ac88bd991e2346b0228c4c4e76a6647fb
AZAT0THT <olivier.simard.2@umontreal.ca>
2024-10-09 15:36:37 -0400

    Ajout options menu Ajout d'elements visuels pour le menu Ajout d'un squelette pour les sous-menus Distinctions entre menu intervenant/resident

--- source/Main.java
+++ source/Main.java
@@ -13,13 +13,19 @@ public class Main {
 
         while (!authReussie) {
             // Menu connexion
-            System.out.println("Bienvenue dans l'application MaVille!");
+            System.out.println("+------------------------------------------+");
+            System.out.println("|       Bienvenue dans l'application       |");
+            System.out.println("|                 MaVille!                 |");
+            System.out.println("+------------------------------------------+");
             System.out.print( "\n" );
             System.out.println("Veuillez vous connecter en tant que :");
             System.out.print( "\n" );
-            System.out.println("1. Résident");
-            System.out.println("2. Intervenant");
-            System.out.println("\n");
+            System.out.println("+---------------+");
+            System.out.println("|1. Résident ~ ~|");
+            System.out.println("|---------------|");
+            System.out.println("|2. Intervenant |");
+            System.out.println("+---------------+");
+            System.out.print("\n");
             System.out.println("3. Quitter l'application");
 
             String choix = scanner.nextLine();
@@ -29,7 +35,6 @@ public class Main {
                 case "1":
                     authReussie = authentifier(residents, scanner);
                     if (authReussie) {
-                        System.out.println("Bienvenue résident!");
                         afficherMenuResident(scanner);
                     } else {
                         System.out.println("Échec de l'authentification, veuillez réessayer");
@@ -39,7 +44,6 @@ public class Main {
                 case "2": 
                     authReussie = authentifier(intervenants, scanner);
                     if (authReussie) {
-                        System.out.println("Bienvenue intervenant!");
                         afficherMenuIntervenant(scanner);
                     } else {
                         System.out.println("Échec de l'authentification, veuillez réessayer");
@@ -48,26 +52,25 @@ public class Main {
                     break;
                   
                 case "3": 
+                    System.out.print("\n\n\n\n\n\n\n");
                     System.out.println("Merci d'avoir utilisé MaVille. À la prochaine!");
+                    System.out.print("\n\n");
                     System.exit(0);
                     break;
 
                 default:
+                    System.out.print("\n\n\n\n\n\n\n");
                     System.out.println("Choix invalide, veuillez choisir une option (ex: 1)");
                     break;
 
             }
         }
-
-
-
-
     }
 
     public static boolean authentifier(String[] utilisateurs, Scanner scanner) {
-
-        System.out.print( "\n\n\n" );
-        System.out.println("Veuillez vous authentifier");
+        // Affichage menu authentification
+        System.out.print("\n\n\n\n\n\n\n");
+        System.out.println("Veuillez vous authentifier"); 
         System.out.print( "\n" );
         System.out.println("Nom d'utilisateur :");
         String username = scanner.nextLine();
@@ -84,17 +87,132 @@ public class Main {
     }
 
     public static void afficherMenuIntervenant(Scanner scanner) {
-        boolean quitter = false;
-        while (!quitter) {
-            
+        boolean valide = false;   // Variable pour sortir de la boucle quand le user entre une entree valide
+
+        // Menu avec les options pour un intervenant
+        System.out.print("\n\n\n\n\n\n\n");
+        System.out.println("Bienvenue intervenant!");
+        System.out.println("Veuillez choisir ce que vous voulez accomplir");
+        System.out.print("\n");
+        System.out.println("1. Consulter les travaux en cours ou à venir");
+        System.out.println("2. Rechercher des travaux");
+        System.out.println("3. Notifications");
+        System.out.println("4. Soumettre une requête de travail");
+        System.out.println("5. Retour au menu principal");
+        System.out.print("\n");
+        System.out.println("6. Quitter l'application");
+
+        while (!valide) {
+            String choix = scanner.nextLine();
+
+            switch(choix) {
+                case "1":
+                    valide = true;
+                    System.out.println("Consultation des travaux...");
+                    break;
+
+                case "2":
+                    valide = true;
+                    System.out.println("Affichage de l'écran de recherche...");
+                    System.out.println();
+                    break;
+
+                case "3":
+                    System.out.println("Affichage des paramètres de notifications...");
+                    System.out.println();
+                    break;
+
+                case "6":
+                    valide = true; 
+                    System.out.print("\n\n\n\n\n\n\n");
+                    System.out.println("Merci d'avoir utilisé MaVille. À la prochaine!");
+                    System.out.print("\n\n");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.print("\n\n\n\n\n\n\n");
+                    System.out.println("Choix invalide, veuillez choisir une option (ex: 1)");
+                    System.out.print("\n");
+                    System.out.println("1. Consulter les travaux en cours ou à venir");
+                    System.out.println("2. Rechercher des travaux");
+                    System.out.println("3. Notifications");
+                    System.out.println("4. Soumettre une requête de travail");
+                    System.out.println("5. Retour au menu principal");
+                    System.out.print("\n");
+                    System.out.println("6. Quitter l'application");
+                    break;
+            }
         }
     }
 
     public static void afficherMenuResident(Scanner scanner) {
-        boolean quitter = false;
-        while (!quitter) {
+        boolean valide = false;   // Variable pour sortir de la boucle quand le user entre une entree valide
+        // Menu avec les options pour un resident
+        System.out.print("\n\n\n\n\n\n\n");
+        System.out.println("Bienvenue résident!");
+        System.out.println("Veuillez choisir ce que vous voulez accomplir");
+        System.out.print("\n");
+        System.out.println("+--------------------------------------------+");
+        System.out.println("|1. Consulter les travaux en cours ou à venir|");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|2. Rechercher des travaux ~ ~ ~ ~ ~ ~ ~ ~ ~ |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|3. Notifications ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~|");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|4. Planification participative ~ ~ ~ ~ ~ ~ ~|");
+        System.out.println("5. Retour au menu principal");
+        System.out.println("      Bienvenue dans l'application               ");
+        System.out.print("\n");
+        System.out.println("6. Quitter l'application");
 
+
+        while (!valide) {
+            String choix = scanner.nextLine();
+
+            switch(choix) {
+                case "1":
+                    valide = true;
+                    System.out.println("^^^^^^^");
+                    break;
+
+                case "2":
+                    valide = true;
+                    System.out.println("Affichage de l'écran de recherche...");
+                    System.out.println("Différents champs de filtrage...");
+                    System.out.println();
+                    break;
+
+                case "3":
+                    valide = true;
+                    System.out.println("Affichage des paramètres de notifications...");
+                    System.out.println("Abonnements actuels...");
+                    System.out.println("Désactiver les notifications...");
+                    System.out.println();
+                    break;
+                
+                case "4":
+                    valide = true;
+                    System.out.println("Affichage des paramètres de notifications...");
+                    System.out.println("Abonnements actuels...");
+                    System.out.println("Désactiver les notifications...");
+                    System.out.println();
+                    break;
+
+                default:
+                    System.out.println("Choix invalide, veuillez choisir une option (ex: 1)");
+                    System.out.print("\n");
+                    System.out.println("1. Consulter les travaux en cours ou à venir");
+                    System.out.println("2. Rechercher des travaux");
+                    System.out.println("3. Notifications");
+                    System.out.println("4. Soumettre une requête de travail");
+                    System.out.println("5. Retour au menu principal");
+                    System.out.print("\n");
+                    System.out.println("6. Quitter l'application");
+                    break;
+            }
         }
+        
     }
 }
 
